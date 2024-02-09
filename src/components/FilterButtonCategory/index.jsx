51fe9a129e4d455754103159937c8b7a8bddf7bf
Erick Padilla <erick100520@gmail.com>
2024-02-09 17:29:17 -0600

    install gh-pages

--- src/components/FilterButtonCategory/index.jsx
+++ src/components/FilterButtonCategory/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 function ButtonCategory({title,setCategoryIn}) {
   return (
     <li
-    className="btn bg-[#12b4fff5] text-black w-32 text-center mx-2 py-1 xl:text-2xl sm:text-lg cursor-pointer font-protest"
+    className="btn bg-[#ff00009d] text-black w-32 text-center mx-2 py-1 xl:text-2xl sm:text-lg cursor-pointer font-protest"
     onClick={() => setCategoryIn(title)}
   >
     {title}
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import nesting from 'tailwindcss/nesting';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    nesting(), // Add nesting plugin before Tailwind
-    tailwindcss({
-      // ... your Tailwind configuration
-    }),
-    autoprefixer(),],
+  plugins: [react(),    tailwindcss({
+    // ... your Tailwind configuration
+  }),],
+  base:'/ZeroPages/',
 })
